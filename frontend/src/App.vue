0e067e10e36601c28d1e42db2166bd898519a511
Jake Chappell <jfchappell@bsu.edu>
2021-11-22 16:58:00 -0500

    Added navbar and page skeletons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/add">About</router-link> |
-      <router-link to="/edit">Edit</router-link> |
-      <router-link to="/delete">Delete</router-link>
+    <Navbar></Navbar>
+    <div class="main-window">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar,
+  }
+}
+</script>
+
 
 <style lang="less">
 #app {
@@ -17,23 +25,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  text-align: left;
-  padding-top: 5px;
-  padding-bottom: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #ffc0cb;
-    }
-    &:hover{
-      color: #ffc0cb;
-    }
-  }
 }
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav class="navbar navbar-light navbar-expand bg-light navigation-clean">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Jokes App</a>
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/add">Add Joke</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/edit">Edit Joke</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/delete">Delete Joke</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style lang="less" scoped>
+.navbar {
+  width: 100%;
+  align-items: center;
+  background: grey;
+  text-align: left;
+  padding: 5px;
+  padding-bottom: 5px;
+  background: #6c5b7b !important;
+
+  .navbar-brand {
+    font-weight: bold;
+    color: white;
+    &:hover{
+        color: #ffc0cb;
+    }
+}
+
+    .nav-item {
+        .nav-link{
+            display: flex;
+            align-items: center;
+            font-weight: normal;
+            color: white;
+            &:hover{
+                color: #ffc0cb;
+                font-weight: bold;
+            }
+        }
+    }
+}
+</style>
